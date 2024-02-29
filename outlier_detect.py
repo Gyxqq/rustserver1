c7baee4397c9635cae35d2801d43b5b3d5def727
gyxqqq <2277904687@qq.com>
2024-02-29 16:58:38 +0800

    完善

---
+++ outlier_detect.py
@@ -0,0 +1,101 @@
+from PIL import Image
+import numpy as np
+from scipy.linalg import solve
+from scipy.spatial.distance import cdist
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+import sys
+
+# 核心算法
+def RLOF(data, k, reg=1e-3):
+    # print("Data shape:", data.shape, file=sys.stderr)  # 输出到 stderr
+    # print("Data values:", data, file=sys.stderr)  # 输出到 stderr
+    n_samples, n_dim = data.shape
+
+    dist = cdist(data, data)
+    indices = np.argsort(dist, axis=1)[:, 1:k+1]
+
+    W = np.zeros((n_samples, n_samples))
+    v = np.ones(k)
+
+    for i, ind in enumerate(indices):
+        A = data[ind]
+        C = A - data[i]
+        G = np.dot(C, C.T)
+        trace = np.trace(G)
+        if trace > 0:
+            R = reg * trace
+        else:
+            R = reg
+        G.flat[:: k + 1] += R
+        w = solve(G, v, assume_a='pos')
+        W[i, ind] = w / np.sum(w)
+    # print(W)
+    # 可达点下标
+    reach = np.where(W>1e-10)
+  
+    n_dist = np.zeros(n_samples)
+    for i in range(n_samples):
+        # 可达点下标中距离最大的点
+        n_dist[i] = np.max(dist[i, reach[1][reach[0]==i]])
+        
+    # 可达局部可达密度
+    lrd = np.zeros(n_samples)
+    for i in range(n_samples):
+        neighbors = reach[1][reach[0]==i]
+        rd = np.maximum(dist[i, neighbors], n_dist[neighbors])
+        lrd[i] = len(neighbors) / (np.sum(rd)+1e-10)
+
+    weight = np.sum(W < 1e-10, axis=0)/n_samples
+
+    # 计算局部离群因子
+    RLOF = np.zeros(n_samples)
+    for i in range(n_samples):
+        RLOF[i] = np.mean(np.outer(lrd[reach[1][reach[0]==i]],weight[reach[1][reach[0]==i]]) / lrd[i])
+
+    return RLOF
+
+# 初始化图片为一维数组
+def convert_to_num(img_path):
+    try:
+        img = Image.open(img_path)
+    except:
+        print('图片路径错误', file=sys.stderr)
+        return
+    img = img.resize((64, 64))
+    img = np.array(img)
+    # print("Image data type:", img.dtype, file=sys.stderr)  # 输出到 stderr
+    # print("Image shape:", img.shape, file=sys.stderr)  # 输出到 stderr
+    # print("Image values:", img, file=sys.stderr)  # 输出到 stderr
+    img = img.reshape(1, -1)
+    return img
+
+# 读取数据集
+def read_dataset():
+    try:
+        data = pd.read_csv('data.csv', header=None)
+    except:
+        print('数据集路径错误', file=sys.stderr)
+        return
+    # print("Dataset data type:", data.dtypes, file=sys.stderr)  # 输出到 stderr
+    # print("Dataset shape:", data.shape, file=sys.stderr)  # 输出到 stderr
+    # print("Dataset values:", data, file=sys.stderr)  # 输出到 stderr
+    return data
+
+# 异常检测
+def outlier_detect(img_path):
+    img = convert_to_num(img_path)
+    data = read_dataset()
+    data = data.iloc[:, :-1]
+    data = pd.concat([data, pd.DataFrame(img)], axis=0)
+    scaler = MinMaxScaler()
+    data = scaler.fit_transform(data)
+    rlof = RLOF(data,5)
+    # 归一化
+    rlof = (rlof - np.min(rlof)) / (np.max(rlof) - np.min(rlof))
+
+    return rlof[-1]
+
+if __name__ == '__main__':
+    for img_path in sys.argv[1:]:
+        print(outlier_detect(img_path))
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #[macro_use]
 extern crate rocket;
 use json;
+// use rand::{self, Rng};
 use rocket::data::ToByteUnit;
 use rocket::fs::FileServer;
 use rocket::tokio::fs::{create_dir_all, File};
@@ -9,6 +10,8 @@ use rocket::Data;
 use rocket::State;
 use sqlite::Connection;
 use std::collections::vec_deque::VecDeque;
+use std::io::Write;
+use std::process::{Command, Termination};
 use std::sync::Arc;
 use std::sync::Mutex;
 use std::thread::sleep;
@@ -16,13 +19,43 @@ use std::time::Duration;
 use std::{env, thread};
 use threadpool::ThreadPool;
 use uuid::Uuid;
-static db: Mutex<i32> = Mutex::new(0);
+
 static config_passwd: &'static str = "123456";
-fn img_process(filename: &String) {
-    println!("img_process:{}", filename);
-    thread::sleep(Duration::from_secs(5));
+fn img_process(filename: &String, db: Arc<Mutex<Connection>>) {
+    println!("img_process start :{}", filename);
+    // let mut rng = rand::thread_rng();
+    // let n: u64 = rng.gen_range(1..100);
+    // thread::sleep(Duration::from_micros(n * 100));
+    let output = Command::new("python")
+        .arg("outlier_detect.py")
+        .arg(filename)
+        .output()
+        .expect("failed to execute process");
+
+    {
+        let conn = db.lock().unwrap();
+        let query = format!(
+            "UPDATE img \n SET outline_score='{}' WHERE filename='{}'",
+            String::from_utf8_lossy(&output.stdout),
+            filename
+        );
+        conn.execute(query).unwrap();
+        let err = String::from_utf8_lossy(&output.stderr);
+        if err != "" {
+            std::fs::create_dir_all("errlog/").unwrap();
+            let name = "errlog/".to_string() + &uuid::Uuid::new_v4().to_string() + ".txt";
+            let mut file = std::fs::File::create(&name).unwrap();
+            file.write_all(err.as_bytes()).unwrap();
+            let query = format!(
+                "UPDATE img SET  err = '{}' WHERE filename='{}'",
+                &name, filename
+            );
+            conn.execute(query).unwrap();
+        }
+        println!("err:{}", err);
+    }
 }
-fn process(deque: &Arc<Mutex<VecDeque<String>>>, num: i32) {
+fn process(deque: &Arc<Mutex<VecDeque<String>>>, num: i32, db: Arc<Mutex<Connection>>) {
     let pool = ThreadPool::new(num as usize);
     loop {
         let empty = {
@@ -31,16 +64,19 @@ fn process(deque: &Arc<Mutex<VecDeque<String>>>, num: i32) {
         };
 
         if empty {
-            sleep(Duration::from_secs(1));
+            sleep(Duration::from_secs(5));
             continue;
         }
+
         let filename = {
             let mut deque = deque.lock().unwrap();
+            println!("now deque len:{}", deque.len());
             deque.pop_front().unwrap()
         };
+        let db0 = Arc::clone(&db);
         pool.execute(move || {
-            img_process(&filename);
-            println!("img_process:{}", &filename)
+            img_process(&filename, db0);
+            println!("img_process over:{}", &filename)
         });
     }
 }
@@ -50,20 +86,23 @@ fn hello() -> Result<String, std::io::Error> {
     Ok(format!("Hello!"))
 }
 #[get("/config/cleardatabase?<password>")]
-fn cleardatabase(password: String) -> Result<String, std::io::Error> {
+async fn cleardatabase(
+    password: String,
+    db: &State<Arc<Mutex<Connection>>>,
+) -> Result<String, std::io::Error> {
     if password != config_passwd {
         return Err(std::io::Error::new(
             std::io::ErrorKind::Other,
             "password error",
         ));
     }
-    let connect = db.lock().unwrap();
-    let conn = Connection::open("img.db").unwrap();
+
+    let conn = db.lock().unwrap();
     conn.execute("DELETE FROM img").unwrap();
     //create img table
     conn.execute(
         "CREATE TABLE IF NOT EXISTS img (id INTEGER PRIMARY KEY,filename TEXT NOT NULL
-        ,usertag TEXT NOT NULL,outline_score TEXT NOT NULL
+        ,usertag TEXT NOT NULL,outline_score TEXT NOT NULL err TEXT
         )",
     )
     .unwrap();
@@ -75,6 +114,7 @@ async fn uploadimg(
     type0: String,
     tag: String,
     data: Data<'_>,
+    db: &State<Arc<Mutex<Connection>>>,
 ) -> Result<String, std::io::Error> {
     let mut filename = Uuid::new_v4().to_string() + "." + &type0;
     filename = "img/".to_string() + &filename;
@@ -92,11 +132,10 @@ async fn uploadimg(
     drop(file);
 
     {
-        let connection = db.lock().unwrap();
-        let conn = Connection::open("img.db").unwrap();
+        let conn = db.lock().unwrap();
         let query = format!(
-            "INSERT INTO img (filename,usertag,outline_score) VALUES ('{}','{}','{}')",
-            filename, tag, "none"
+            "INSERT INTO img (filename,usertag,outline_score,err) VALUES ('{}','{}','{}','{}')",
+            filename, tag, "none", "none"
         );
         conn.execute(query).unwrap();
     }
@@ -105,6 +144,7 @@ async fn uploadimg(
         deque.push_back(filename.clone());
         println!("deque add : {}", deque.back().unwrap());
     }
+
     let json = json::object! {
         "filename" => filename,
         "usertag" => tag
@@ -112,9 +152,11 @@ async fn uploadimg(
     Ok(format!("{}", json.dump()))
 }
 #[get("/getimgstat_bytag?<tag>")]
-fn getimgstat_bytag(tag: String) -> Result<String, std::io::Error> {
-    let connect = db.lock().unwrap();
-    let conn = Connection::open("img.db").unwrap();
+async fn getimgstat_bytag(
+    tag: String,
+    db: &State<Arc<Mutex<Connection>>>,
+) -> Result<String, std::io::Error> {
+    let conn = db.lock().unwrap();
     let query = format!("SELECT * FROM img WHERE usertag='{}'", tag);
     let mut stmt = conn.prepare(query).unwrap();
     let mut imglist = json::JsonValue::new_array();
@@ -126,19 +168,23 @@ fn getimgstat_bytag(tag: String) -> Result<String, std::io::Error> {
         let filename: String = stmt.read(1).unwrap();
         let usertag: String = stmt.read(2).unwrap();
         let outline_score: String = stmt.read(3).unwrap();
+        let err: String = stmt.read(4).unwrap();
         let img = json::object! {
             "filename" => filename,
             "usertag" => usertag,
-            "outline_score" => outline_score
+            "outline_score" => outline_score,
+            "err" => err
         };
         imglist.push(img).unwrap();
     }
     Ok(format!("{}", imglist.dump()))
 }
 #[get("/getimgstat_byfilename?<filename>")]
-fn getimgstat_byfilename(filename: String) -> Result<String, std::io::Error> {
-    let connect = db.lock().unwrap();
-    let conn = Connection::open("img.db").unwrap();
+async fn getimgstat_byfilename(
+    filename: String,
+    db: &State<Arc<Mutex<Connection>>>,
+) -> Result<String, std::io::Error> {
+    let conn = db.lock().unwrap();
     let query = format!("SELECT * FROM img WHERE filename='{}'", filename);
     let mut stmt = conn.prepare(query).unwrap();
 
@@ -149,44 +195,55 @@ fn getimgstat_byfilename(filename: String) -> Result<String, std::io::Error> {
     let filename: String = stmt.read(1).unwrap();
     let usertag: String = stmt.read(2).unwrap();
     let outline_score: String = stmt.read(3).unwrap();
+    let err: String = stmt.read(4).unwrap();
     let img = json::object! {
         "filename" => filename,
         "usertag" => usertag,
-        "outline_score" => outline_score
+        "outline_score" => outline_score,
+        "err" => err
+
     };
     Ok(format!("{}", img.dump()))
 }
 #[get("/getdequelen")]
-fn getdequelen(deque: &State<Arc<Mutex<VecDeque<String>>>>) -> Result<String, std::io::Error> {
-    let deque = deque.lock().unwrap();
+async fn getdequelen(db: &State<Arc<Mutex<Connection>>>) -> Result<String, std::io::Error> {
+    let conn = db.lock().unwrap();
+    //seclect count  outline_score=none
+    let query = format!("SELECT count(*) FROM img WHERE outline_score='none'");
+    let mut stmt = conn.prepare(query).unwrap();
+    stmt.next().unwrap();
+    let count: i64 = stmt.read(0).unwrap();
     //json
     let json = json::object! {
-        "len" => deque.len()
+        "dequelen" => count
     };
     Ok(format!("{}", json.dump()))
+    // let count = stmt.next().unwrap();
+
 }
 #[launch]
 fn rocket() -> _ {
-    {
-        env::set_var("ROCKET_LOG", "trace");
-        let mut connect = db.lock().unwrap();
-
-        let conn = Connection::open("img.db").unwrap();
-        //open img table or create img table
-        conn.execute(
-            "CREATE TABLE IF NOT EXISTS img (id INTEGER PRIMARY KEY,filename TEXT NOT NULL
-            ,usertag TEXT NOT NULL,outline_score TEXT NOT NULL
+    env::set_var("ROCKET_LOG", "trace");
+    // let mut connect = db.lock().unwrap();
+    // let
+    let conn = Connection::open("img.db").unwrap();
+    //open img table or create img table
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS img (id INTEGER PRIMARY KEY,filename TEXT NOT NULL
+            ,usertag TEXT NOT NULL,outline_score TEXT ,err TEXT
             )",
-        )
-        .unwrap();
-        println!("database open success");
-    }
+    )
+    .unwrap();
+    println!("database open success");
+    let conn = Arc::new(Mutex::new(Connection::open("img.db").unwrap()));
     let deque = Arc::new(Mutex::new(VecDeque::<String>::new()));
     let deque_clone = Arc::clone(&deque);
-    thread::spawn(move || process(&deque_clone, 4));
+    let conn_clone = Arc::clone(&conn);
+    thread::spawn(move || process(&deque_clone, 4, conn_clone));
 
     rocket::build()
         .manage(deque)
+        .manage(conn)
         .mount("/", routes![hello])
         .mount("/", rocket::routes![uploadimg])
         .mount("/res", FileServer::from("res/"))
